4dce3b1991a6d4d12a9e4d9212849e2dee6de9ff
Giorgos Tsolakidis <rossakis@hotmail.com>
2024-10-18 16:24:45 +0300

    Created basic OpenGl window

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,79 @@
 #include <GLFW/glfw3.h>
-#include <GL/gl.h>
 #include <imgui.h>
+#include <glad/gl.h>
 
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
 
-class HelloTriangleApplication {
-public:
-    const uint32_t WIDTH = 800;
-    const uint32_t HEIGHT = 600;
+#define numVAOs 1 //number of vertices
 
-    void run() {
-        initWindow();
-        initVulkan();
-        mainLoop();
-        cleanup();
+GLuint renderingProgram;
+GLuint vertexBuffer[numVAOs];
+
+//Initializes the OpenGL pipeline (vertex shader, fragment shader, etc...)
+void init(GLFWwindow* window) {
+
+}
+
+void display(GLFWwindow* window, double currentTime) {
+    glClearColor(0.0, 0.0, 0.0, 1.0);// Set clear color to black
+    glClear(GL_COLOR_BUFFER_BIT);// Set "clear" color to black
+    //glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //clear both the color and depth buffer
+    //when depth buffer is cleared, it goes to 1 (farthest from the camera)
+}
+
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 720;
+
+GLFWwindow* SetScreenMode(bool isFullScreen) {
+
+    if (isFullScreen) {
+        GLFWmonitor* monitor = glfwGetPrimaryMonitor();//get current main monitor
+        const GLFWvidmode* mode = glfwGetVideoMode(monitor); //set full screen
+        return glfwCreateWindow(mode->width, mode->height, "Shader Ninja", monitor, NULL);
     }
-
-private:
-    GLFWwindow* window;
-
-    void initWindow()
-    {
-        //Initialize glfw library
-        glfwInit();
-
-        //Define a window that is not OpenGl dependendent
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-
-        //Create the window
-        window = glfwCreateWindow(WIDTH, HEIGHT, "Shader Ninja", nullptr, nullptr);
-    }
-    void initVulkan()
-    {
-
-    }
-
-    void mainLoop()
-    {
-        while(!glfwWindowShouldClose(window))
-        {
-            glfwPollEvents();
-        }
-    }
-
-    void cleanup()
-    {
-        glfwDestroyWindow(window);
-        glfwTerminate();
-    }
-};
+    return glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Shader Ninja", NULL, NULL);
+}
 
 int main() {
-    HelloTriangleApplication app;
 
-    try {
-        app.run();
-    }
-    catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-        return EXIT_FAILURE;
+    //Initialize the GLFW library
+    if (!glfwInit()) {
+        exit(EXIT_FAILURE);
     }
 
-    return EXIT_SUCCESS;
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    GLFWwindow* window = SetScreenMode(false);
+
+    if (!window) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwMakeContextCurrent(window);
+
+    //Init Glad and Load OpenGL loader through it
+    if (gladLoadGL(glfwGetProcAddress) == 0) {
+        throw std::runtime_error("Failed to load OpenGL context\n");
+    }
+
+    //Enable V-Sync
+    glfwSwapInterval(1);// 1 = number of screen updates before the GLFW buffers get swapped
+
+    init(window);
+
+    //Wait until user closes the window
+    while (!glfwWindowShouldClose(window)) {
+        display(window, glfwGetTime());
+        glfwSwapBuffers(window);//GLFW windows are double-buffered (meaning there are two color buffers)
+        glfwPollEvents();//poll event for input (mouse, keyboard, joystick, etc...)
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+
 }
